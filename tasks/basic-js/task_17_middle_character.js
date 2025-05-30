07fe0487c73359acee4fda304f33a5ac773cdbf0
Curtislike <anna-olesh@mail.ru>
2025-05-30 13:23:13 +0300

    add tasks descriptions

--- tasks/basic-js/task_17_middle_character.js
+++ tasks/basic-js/task_17_middle_character.js
@@ -1,4 +1,11 @@
 /**
- * Условие задачи: https://www.codewars.com/kata/56747fd5cb988479af000028
- *
+You are going to be given a non-empty string. Your job is to return the middle character(s) of the string.
+
+If the string's length is odd, return the middle character.
+If the string's length is even, return the middle 2 characters.
+Examples:
+"test" --> "es"
+"testing" --> "t"
+"middle" --> "dd"
+"A" --> "A"
  */
--- tasks/basic-js/task_7_function_within_a_function.js
+++ tasks/basic-js/task_7_function_within_a_function.js
@@ -1,4 +1,6 @@
 /**
- * Условие задачи: https://www.codewars.com/kata/53844152aa6fc137d8000589
- *
+Given an input n, write a function always that returns a function which returns n. Ruby should return a lambda or a proc.
+
+var three = always(3);
+three(); // returns 3
  */
