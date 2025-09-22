3569866297d68699d877a5e7e9d00551d351a6fe
Kostya Betenya <betenya.kostya@mail.ru>
2025-09-22 16:32:10 +0300

    feat: create custom Map

--- tasks/basic-js/task_11_map.js
+++ tasks/basic-js/task_11_map.js
@@ -2,3 +2,34 @@
  * Реализовать map вручную
  *
  */
+
+class Map {
+    #obj = [];
+
+    set(key, value) {
+        const existIndex = this.#obj.findIndex(item => item.key === key);
+        if (existIndex !== -1) {
+            this.#obj[existIndex].value = value;
+        } else {
+            this.#obj.push({key, value});
+        }
+    }
+
+    get(key) {
+        const existIndex = this.#obj.findIndex(item => item.key === key);
+        return existIndex === -1 ? undefined : this.#obj[existIndex].value;
+    }
+
+    has(key) {
+        const existIndex = this.#obj.findIndex(item => item.key === key);
+        return existIndex !== -1;
+    }
+
+    get size() {
+        return this.#obj.length;
+    }
+
+    clear() {
+        this.#obj = [];
+    }
+}
