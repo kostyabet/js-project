b674e3b6ef9d85c12b3279315682a5875cd322c8
Kostya Betenya <betenya.kostya@mail.ru>
2025-09-23 13:16:26 +0300

    fix task 23

--- tasks/basic-js/task_23_settimeout_mistake.js
+++ tasks/basic-js/task_23_settimeout_mistake.js
@@ -3,8 +3,24 @@
  *
  */
 
+for (var i = 0; i < 10; i++) {
+    setTimeout(function () {
+        console.log('index', i);
+    }, 3000);
+}
+
+// Variant 1
 for (let i = 0; i < 10; i++) {
   setTimeout(function () {
     console.log('index', i);
   }, 3000);
+}
+
+// Variant 2
+for (var i = 0; i < 10; i++) {
+    (function (number){
+        setTimeout(function () {
+            console.log('index', number);
+        }, 3000);
+    })(i)
 }
