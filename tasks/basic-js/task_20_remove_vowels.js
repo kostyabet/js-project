047a311b7627931ee4ea01a55565f360ba4bb3d8
Kostya Betenya <betenya.kostya@mail.ru>
2025-09-23 09:19:24 +0300

    feat: complete task 20

--- tasks/basic-js/task_20_remove_vowels.js
+++ tasks/basic-js/task_20_remove_vowels.js
@@ -2,3 +2,13 @@
  * Написать функцию, которая удаляет все гласные из строки, возвращает новую строку
  *
  */
+
+function removeVowels(str) {
+    const vowels = ['a', 'e', 'i', 'o', 'u', 'y']
+    const lStr = str.toLowerCase();
+    let result = '';
+    for (let i = 0; i < lStr.length; i++) {
+        if (!vowels.includes(lStr[i])) result += lStr[i];
+    }
+    return result;
+}
