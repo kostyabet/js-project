3a227c1b13381414f7190ad6048861bade0fced9
Kostya Betenya <betenya.kostya@mail.ru>
2025-09-23 09:15:36 +0300

    feat: complete task 19

--- tasks/basic-js/task_19_is_palindrome.js
+++ tasks/basic-js/task_19_is_palindrome.js
@@ -2,3 +2,14 @@
  * Написать функцию, которая определяет, является ли строка палиндромом
  *
  */
+
+function isPalindrome(str) {
+    const left = str.slice(0, str.length / 2);
+    const right = str.slice(str.length / 2);
+    if (left.length !== right.length) return false;
+    for (let i = 0; i < left.length; i++) {
+        if (left[i] !== right[left.length - i - 1])
+            return false;
+    }
+    return true;
+}
