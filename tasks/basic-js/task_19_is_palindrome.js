f153a3a6e0594ab6ebe82c1a75146273de3759c0
Kostya Betenya <betenya.kostya@mail.ru>
2025-09-23 13:18:32 +0300

    fix task 19

--- tasks/basic-js/task_19_is_palindrome.js
+++ tasks/basic-js/task_19_is_palindrome.js
@@ -4,12 +4,8 @@
  */
 
 function isPalindrome(str) {
-    const left = str.slice(0, str.length / 2);
-    const right = str.slice(str.length / 2);
-    if (left.length !== right.length) return false;
-    for (let i = 0; i < left.length; i++) {
-        if (left[i] !== right[left.length - i - 1])
-            return false;
-    }
-    return true;
+   for (let i = 0; i < str.length / 2; i++) {
+       if (str[i] !== str[str.length - 1 - i]) return false;
+   }
+   return true;
 }
