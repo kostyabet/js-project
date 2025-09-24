b88a091fd760c6bdf564d14f6ba12ad47a39775e
Kostya Betenya <betenya.kostya@mail.ru>
2025-09-24 09:56:06 +0300

    feat: complete task 29

--- tasks/advanced-js/task_29_partial.js
+++ tasks/advanced-js/task_29_partial.js
@@ -9,3 +9,14 @@ function sum(a, b, c) {
 const addFive = partial(sum, 2, 3);
 addFive(10) → 15  // 2 + 3 + 10
 */
+
+function sum(a, b, c) {
+    return a + b + c;
+}
+
+function partial(fn, ...args) {
+    return (...newArgs) => fn.apply(this, [...args, ...newArgs]);
+}
+
+const addFive = partial(sum, 2, 3);
+console.log(addFive(10));
