d719148252fe7b0855186bbbf4fbfd98992dccd3
Kostya Betenya <betenya.kostya@mail.ru>
2025-09-24 10:13:28 +0300

    feat: complete task 32

--- tasks/advanced-js/task_32_animals_and_inheritance.js
+++ tasks/advanced-js/task_32_animals_and_inheritance.js
@@ -38,3 +38,42 @@ Dogs have an identical introduce/Introduce method as any other animal, but they 
 called greetMaster/GreetMaster which accepts no arguments and returns "Hello (insert_master_name_here)" (of course not the literal string but replace the 
     (insert_master_name_here) with the name of the dog's master).
 */
+
+class Animal {
+    constructor(name, age, legs, species, status) {
+        this.name = name;
+        this.age = age;
+        this.legs = legs;
+        this.species = species;
+        this.status = status;
+    }
+    introduce() {
+        return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
+    }
+}
+
+class Shark extends Animal {
+    constructor(name, age, status) {
+        super(name, age, 0, "shark", status);
+    }
+}
+
+class Cat extends Animal {
+    constructor(name, age, status) {
+        super(name, age, 4, "cat", status);
+    }
+
+    introduce() {
+        return super.introduce() + "  Meow meow!";
+    }
+}
+
+class Dog extends Animal {
+    constructor(name, age, status, master) {
+        super(name, age, 4, "dog", status);
+        this.master = master;
+    }
+    greetMaster() {
+        return `Hello ${this.master}`
+    }
+}
