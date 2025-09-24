8f39baca24c9929e3bf3d08d41c1980b042ddca2
Kostya Betenya <betenya.kostya@mail.ru>
2025-09-24 09:28:57 +0300

    feat: complete task 26

--- tasks/dom-and-events/task_26_theme.js
+++ tasks/dom-and-events/task_26_theme.js
@@ -14,3 +14,26 @@
 Фон и текст сайта должны менять стили в зависимости от класса (можно добавить простые CSS-стили для светлой и тёмной темы)
 
 */
+
+const container = document.getElementById('container');
+let theme = localStorage.getItem('theme') || 'light';
+
+const button = document.createElement('button');
+renderButtonContent();
+renderTheme();
+function renderButtonContent() {
+    button.textContent = theme === 'light' ? 'Светлая тема' : 'Тёмная тема';
+}
+function renderTheme() {
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.remove(theme === 'light' ? 'dark' : 'light');
+    body.classList.add(theme === 'light' ? 'light' : 'dark');
+}
+button.addEventListener('click', (e) => {
+    theme = theme === 'light' ? 'dark' : 'light';
+    localStorage.setItem('theme', theme);
+    renderButtonContent();
+    renderTheme();
+})
+
+container.appendChild(button);
