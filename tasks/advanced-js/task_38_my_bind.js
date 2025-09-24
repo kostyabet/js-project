efe5383a008931ffb8c133fb1b9163f25d2aad2b
Kostya Betenya <betenya.kostya@mail.ru>
2025-09-24 11:10:22 +0300

    feat: complete task 38

--- tasks/advanced-js/task_38_my_bind.js
+++ tasks/advanced-js/task_38_my_bind.js
@@ -9,3 +9,18 @@ const person = { name: 'Alice' };
 const boundGreet = greet.myBind(person, 'Hello');
 boundGreet(); // Output: Alice says: Hello
 */
+
+Function.prototype.myBind = function(context, ...boundArgs) {
+    const fn = this;
+
+    return function() {
+        return fn.call(context, boundArgs);
+    }
+}
+function greet(message) {
+    console.log(this.name + ' says: ' + message);
+}
+
+const person = { name: 'Alice' };
+const boundGreet = greet.myBind(person, 'Hello');
+boundGreet()
