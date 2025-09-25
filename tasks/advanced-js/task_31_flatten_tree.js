36f4effbdb2e7acac5258f23fe4fef774db99aeb
Kostya Betenya <betenya.kostya@mail.ru>
2025-09-25 09:01:24 +0300

    fix: task 31

--- tasks/advanced-js/task_31_flatten_tree.js
+++ tasks/advanced-js/task_31_flatten_tree.js
@@ -22,8 +22,24 @@ const tree = {
 
 function flattenTree(tree) {
     const result = [tree.value];
-    for (let i = 0; i < tree.children.length; i++) {
-        result.push(...flattenTree(tree.children[i])) ;
+    for (const child of tree.children) {
+        result.push(...flattenTree(child)) ;
     }
     return result;
 }
+
+const tree = {
+    value: 1,
+    children: [
+        { value: 2, children: [] },
+        {
+            value: 3,
+            children: [
+                { value: 4, children: [] },
+                { value: 5, children: [] }
+            ]
+        }
+    ]
+};
+
+console.log(flattenTree(tree)) // [1, 2, 3, 4, 5]
