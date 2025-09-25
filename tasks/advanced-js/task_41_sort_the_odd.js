96506d59be9fc90ec2eebb58b22c88ca14140813
Kostya Betenya <betenya.kostya@mail.ru>
2025-09-25 09:21:06 +0300

    feat: add new solution for task 41

--- tasks/advanced-js/task_41_sort_the_odd.js
+++ tasks/advanced-js/task_41_sort_the_odd.js
@@ -8,6 +8,7 @@ Examples
 [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
 */
 
+// Variant 1
 function bubbleOddSort(array) {
     const arr = [...array];
     for (let i = 0; i < arr.length; i++) {
@@ -22,6 +23,21 @@ function bubbleOddSort(array) {
         }
     }
     return arr;
+}
+
+// Variant 2
+function bubbleOddSort(array) {
+    const sortedOdd = [...array]
+        .filter(item => item % 2 !== 0)
+        .sort((a, b) => a - b);
+
+    let sortedId = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 2 === 1) {
+            array[i] = sortedOdd[sortedId++];
+        }
+    }
+    return array;
 }
 
 console.log(bubbleOddSort([7, 1]));
