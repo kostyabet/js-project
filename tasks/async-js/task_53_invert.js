11854c598569fb99d51548877053bdd1f1148839
Kostya Betenya <betenya.kostya@mail.ru>
2025-09-26 14:09:36 +0300

    feat: add new resolve for task 53

--- tasks/async-js/task_53_invert.js
+++ tasks/async-js/task_53_invert.js
@@ -4,6 +4,7 @@
 invert({ a: 1, b: 2, c: 1 }) // → { 1: ['a','c'], 2: ['b'] }
 */
 
+// Variant 1
 function invert(obj) {
     const result = {};
     for (let key in obj) {
@@ -14,4 +15,16 @@ function invert(obj) {
     return result;
 }
 
+// Variant 2
+function invert(obj) {
+    const result = {}
+    const values = Object.entries(obj);
+    for (let i = 0; i < values.length; i++) {
+        const current = values[i];
+        if (result[current[1]] === undefined)
+            result[current[1]] = [];
+        result[current[1]].push(current[0]);
+    }
+    return result;
+}
 console.log(invert({ a: 1, b: 2, c: 1 }));
