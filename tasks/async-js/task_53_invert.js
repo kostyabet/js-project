ecba68a5a87850b20cca0e0c2d6b075f4e6f37de
Kostya Betenya <betenya.kostya@mail.ru>
2025-09-26 10:01:18 +0300

    feat: complete task 53

--- tasks/async-js/task_53_invert.js
+++ tasks/async-js/task_53_invert.js
@@ -3,3 +3,15 @@
 Если значения повторяются, сохранить массив ключей:
 invert({ a: 1, b: 2, c: 1 }) // → { 1: ['a','c'], 2: ['b'] }
 */
+
+function invert(obj) {
+    const result = {};
+    for (let key in obj) {
+        if (result[obj[key]] === undefined)
+            result[obj[key]] = [];
+        result[obj[key]].push(key);
+    }
+    return result;
+}
+
+console.log(invert({ a: 1, b: 2, c: 1 }));
