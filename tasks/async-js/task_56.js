5766c5f7fd277e89ab691d7bc9bad4ef69f0eba9
Kostya Betenya <betenya.kostya@mail.ru>
2025-09-26 14:15:33 +0300

    fix: task 56

--- tasks/async-js/task_56.js
+++ tasks/async-js/task_56.js
@@ -6,7 +6,13 @@
 function deepClone(obj) {
     const result = {};
     for(let key in obj) {
-        if (typeof obj[key] === 'object') {
+        if (
+            typeof obj[key] === 'object' &&
+            !(obj[key] instanceof Array) &&
+            !(obj[key] instanceof Set) &&
+            !(obj[key] instanceof Map) &&
+            !(obj[key] === null)
+        ) {
             result[key] = deepClone(obj[key]);
         } else {
             result[key] = obj[key];
@@ -16,13 +22,13 @@ function deepClone(obj) {
 }
 
 const obj1 = {
-    v1: 1,
-    v2: 2,
-    v3: {
-        v4: () => {
-            return "slkfsdf";
-        },
-        v5: 1,
+    v1: [1, 2, 3],
+    v2: new Set([1, 2, 3]),
+    v3: new Map(),
+    v4: null,
+    v5: {
+        v6: 1,
+        v7: 12,
     }
 }
 console.log(obj1, deepClone(obj1));
