994ccbb1d23bf1b532b2933e5217057d1b1ed863
Kostya Betenya <betenya.kostya@mail.ru>
2025-09-26 09:35:27 +0300

    feat: complete task 51

--- tasks/async-js/task_51_sum_of_digits.js
+++ tasks/async-js/task_51_sum_of_digits.js
@@ -10,3 +10,20 @@ Examples
 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
 */
+
+function digitsSum(num) {
+    const curSum = (current) => {
+        if (current <= 0) return 0;
+        return current % 10 + curSum(Math.floor(current / 10));
+    }
+    let result = curSum(num);
+    while (result / 10 > 1) {
+        result = curSum(result);
+    }
+    return result;
+}
+
+console.log(digitsSum(16)); // 7
+console.log(digitsSum(942)); // 6
+console.log(digitsSum(132189)); // 6
+console.log(digitsSum(493193)); // 2
